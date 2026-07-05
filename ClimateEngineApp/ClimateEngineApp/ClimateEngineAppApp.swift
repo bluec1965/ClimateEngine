a739df12aef1ce97c87fff5dd56a4a9f0daace5d
Alois Carnier <bluec@gmx.ch>
2026-07-05 22:49:16 +0200

    feat(app): create native macOS host application

---
+++ ClimateEngineApp/ClimateEngineApp/ClimateEngineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateEngineAppApp.swift
+//  ClimateEngineApp
+//
+//  Created by Alois Carnier on 05.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ClimateEngineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimateEngineApp/ClimateEngineAppTests/ClimateEngineAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ClimateEngineAppTests.swift
+//  ClimateEngineAppTests
+//
+//  Created by Alois Carnier on 05.07.2026.
+//
+
+import Testing
+@testable import ClimateEngineApp
+
+struct ClimateEngineAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
